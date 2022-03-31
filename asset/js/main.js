bc8ef531e94c8c1e646b9466797902b4741dae9a
Simone Saputo <simonesaputo0@gmail.com>
2022-03-31 16:37:40 +0200

    first-commit

---
+++ asset/js/main.js
@@ -0,0 +1,16 @@
+var app = new Vue(
+    {
+        el: '#root',
+        
+        data: {
+            media: 'https://source.unsplash.com/random/?space',
+            classiImg: 'middle',
+            classContainer: 'h_100vh'
+        },
+        
+        methods: {
+
+        },
+    }, 
+    
+)
