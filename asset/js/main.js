bf1c201f02a8131f1b1207aab18a4c982e2611df
Simone Saputo <simonesaputo0@gmail.com>
2022-03-31 21:59:20 +0200

    third-commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -13,24 +13,74 @@ var app = new Vue(
     {
         el: '#root',
         
+        // created() {
+        //     setTimeout(3000)
+        // },
+
         data: {
+            // media: [
+            //     {
+            //     id: 1,
+        mediaImg: ['https://source.unsplash.com/random/?space',
+                    'https://source.unsplash.com/random/?nature',
+                    'https://source.unsplash.com/random/?space',
+                    'https://source.unsplash.com/random/?space',
+                    'https://source.unsplash.com/random/?space'
+            ],
             classiImg: 'middle',
             classContainer: 'h_100vh',
-            media: [
-                'https://source.unsplash.com/random/?space',
-                // 'https://source.unsplash.com/random/?space',
-                // 'https://source.unsplash.com/random/?space',
-                // 'https://source.unsplash.com/random/?space',
-                // 'https://source.unsplash.com/random/?space',  
-            ],
+            // counter: 0,
+            newArray: [],
+            
         },
         
 
         methods: {
-            getRightMethod: function(element){
+            
+            // Funzione per calcolare l'indice dell'array
+            getIndexArrayCalcuate: function(element, i){
+                this.mediaImg.forEach(
+                    (element, index) => {
+                        console.log(index)
+                        this.newArray += element.index
+                        return this.newArray
+                    }
+                )
+              
+            },
 
-            }
+            // Funzione per aggiungere un elemento all'array
+            getRight: function(element, index){
+                let counter = this.mediaImg++
+                this.newArray.forEach(
+                    (elmenet, index) => {
+                        if(true && counter <=  this.mediaImg.length){
+                            index++
+                            console.log(index)
+                        } return this.newArray
+                        
+                        
+                    }
+                ) 
+                 
+            },
+
+            // Funzione per togliere un elemento all'array
+            getLeft: function(element, index){
+                let counter = this.mediaImg--
+                this.newArray.forEach(
+                    (elmenet, index) => {
+                        if(true && counter >=  this.mediaImg.length){
+                            counter--
+                            console.log(counter)
+                        } 
+                        return this.newArray
+                    }
+                ) 
+                 
+            },
+           
+        }
         },
-    }, 
-    
+       
 )
