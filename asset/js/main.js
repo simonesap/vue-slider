78252e529af8fe17052e478ed4b134c442595a9c
Simone Saputo <simonesaputo0@gmail.com>
2022-04-01 10:57:39 +0200

    fourth-commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,74 +12,55 @@
 var app = new Vue(
     {
         el: '#root',
-        
-        // created() {
-        //     setTimeout(3000)
-        // },
 
         data: {
-            // media: [
-            //     {
-            //     id: 1,
-        mediaImg: ['https://source.unsplash.com/random/?space',
+            mediaImg: ['https://source.unsplash.com/random/?space',
                     'https://source.unsplash.com/random/?nature',
-                    'https://source.unsplash.com/random/?space',
-                    'https://source.unsplash.com/random/?space',
-                    'https://source.unsplash.com/random/?space'
+                    'https://source.unsplash.com/random/?city',
+                    'https://source.unsplash.com/random/?fruit',
+                    'https://source.unsplash.com/random/?dog'
             ],
             classiImg: 'middle',
             classContainer: 'h_100vh',
-            // counter: 0,
-            newArray: [],
+            counterImg: 0,
             
         },
         
+        created() {
+            this.nextPhotoTiming();
+        },
 
         methods: {
-            
-            // Funzione per calcolare l'indice dell'array
-            getIndexArrayCalcuate: function(element, i){
-                this.mediaImg.forEach(
-                    (element, index) => {
-                        console.log(index)
-                        this.newArray += element.index
-                        return this.newArray
-                    }
-                )
-              
+
+            // Funzione per spostarsi di un elemento a destra nell''array
+            nextPhotoRight: function(){
+                this.counterImg += 1;
+                if(this.counterImg > (this.mediaImg.length -1)){
+                    this.counterImg = 0;
+                }
             },
 
-            // Funzione per aggiungere un elemento all'array
-            getRight: function(element, index){
-                let counter = this.mediaImg++
-                this.newArray.forEach(
-                    (elmenet, index) => {
-                        if(true && counter <=  this.mediaImg.length){
-                            index++
-                            console.log(index)
-                        } return this.newArray
-                        
-                        
-                    }
-                ) 
-                 
+            // Funzione per spostarsi di un elemento a sinistra nell''array
+            nextPhotoLeft: function(){
+                this.counterImg -= 1;
+                if(this.counterImg < 0){
+                    this.counterImg = this.mediaImg.length - 1;
+                }
             },
 
-            // Funzione per togliere un elemento all'array
-            getLeft: function(element, index){
-                let counter = this.mediaImg--
-                this.newArray.forEach(
-                    (elmenet, index) => {
-                        if(true && counter >=  this.mediaImg.length){
-                            counter--
-                            console.log(counter)
-                        } 
-                        return this.newArray
-                    }
-                ) 
-                 
+            // Funzione per l'intervallo di tempo
+            nextPhotoTiming: function(){
+                setInterval(() => {
+                    this.nextPhotoRight();
+                },3000);
             },
-           
+
+            // Funzione per vedere la foto cliccata secondo l'indice comune di foto e pallino
+            clickDots: function(indexPhoto){
+                console.log(indexPhoto);
+                    this.counterImg = indexPhoto;
+            }
+    
         }
         },
        
